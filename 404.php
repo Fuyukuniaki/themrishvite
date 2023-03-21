39d6b0c971ca140c9923fd7b532354694fd77919
Fuyukuniaki <info@a5-w.com>
2023-03-22 00:38:27 +0900

    [Modify]Sass

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main class="l-main">
+	<article class="p-text__404">
+		<h2 class="c-text__404">404</h2>
+		<p class="c-text__plain">
+			お探しのページは、見つかりませんでした。<br>
+			<span class="c-text__aside">netの土の中にBugのように潜り込んでいるようです。</span>
+		</p>
+		<p class="c-text__plain">
+			一旦、てっぺんまで登るのもいいかも知れません<a href="#" class="c-button__backtofront"></a>
+		</p>
+	</article>
+</main>
+
+<?php get_footer(); ?>
---
+++ front-page.php
@@ -0,0 +1,99 @@
+<?php get_header(); ?>
+	<main class="l-main p-front-page">
+		<section class="p-front-page__main-visual">
+			<h2 class="c-front-page__main-visual--text">好きな仕事を<br>好きな人と好きな人のために</h2>
+		</section>
+		<article class="p-front-page__parts">
+			<h3 class="c-front-page__header">Bugについて</h3>
+			<p class="c-front-page__text">当店のこだわりは、お客様に最高品質のバイクとバイク雑貨を提供することです。私たちは、長年にわたって蓄積された経験と知識を活かし、お客様に満足していただけるような製品を厳選して取り扱っています。バイクに関しては私たちは最高品質のメーカーのみを扱い......</p>
+			<a class="p-front-page__button"><span class="c-front-page__button--text">詳細はこちら</span></a>
+		</article>
+		<article class="p-front-page__parts">
+			<h3 class="c-front-page__header">お知らせ</h3>
+			<ol class="p-front-page__list">
+				<li class="c-front-page__list">
+					<ol class="p-front-page__list--line">
+						<li class="c-front-page__list--parts">20230113</li><li class="c-front-page__list--parts">お知らせ</li><li class="c-front-page__list--parts">雑誌「〇〇」に掲載していただきました</li>
+					</ol>
+				</li>
+				<li class="c-front-page__list">
+					<ol class="p-front-page__list--line">
+						<li class="c-front-page__list">20230109</li><li class="c-front-page__list--parts">お知らせ</li><li class="c-front-page__list--parts">ハンドメイド作家さんの募集</li>
+					</ol>
+				</li>
+				<li class="c-front-page__list">
+					<ol class="p-front-page__list--line">
+						<li class="c-front-page__list">20230107</li><li class="c-front-page__list--parts">イベント</li><li class="c-front-page__list--parts">〇〇セミナーのお知らせ</li>
+					</ol>
+				</li>
+				<li class="c-front-page__list">
+					<ol class="p-front-page__list--line">
+						<li class="c-front-page__list">20221214</li><li class="c-front-page__list--parts">イベント</li><li class="c-front-page__list--parts">〇〇教室のお知らせ</li>
+					</ol>
+				</li>
+				<li class="c-front-page__list">
+					<ol class="p-front-page__list--line">
+						<li class="c-front-page__list">20221123</li><li class="c-front-page__list--parts">お知らせ</li><li class="c-front-page__list--parts">ホームページ公開しました</li>
+					</ol>
+				</li>
+			</ol>
+			<a class="c-front-page__button"><span class="c-front-page__button--text">詳細はこちら</span></a>
+		</article>
+		<article class="p-front-page__parts">
+			<h3 class="c-front-page__header">サービス</h3>
+			<a class="p-front-page__banner">
+				<span class="c-front-page__banner--title">バイク販売</span>
+			</a>
+			<a class="p-front-page__banner">
+				<span class="c-front-page__banner--title">革工房</span>
+			</a>
+			<a class="p-front-page__banner">
+				<span class="c-front-page__banner--title">雑貨販売</span>
+			</a>
+			<a class="p-front-page__banner">
+				<span class="c-front-page__banner--title">古着販売</span>
+			</a>
+			<a class="c-front-page__button"><span class="c-front-page__button--text">サービス一覧を見る</span></a>
+		</article>
+		<article class="p-front-page__parts">
+			<h3 class="c-front-page__header">ブログ</h3>
+			<a class="p-front-page__banner">
+				<figure class="c-front-page__banner--image">
+					<img src="/images/no-image.png">
+					<figcaption class="p-front-page__banner--text">
+						<ul class="p-front-page__banner--data">
+							<li class="c-front-page__banner--date">2022/01/13</li>
+							<li class="c-front-page__banner--title">新車入荷しました</li>
+						</ul>
+					</figcaption>
+				</figure>
+				<span class="c-front-page__button">詳細はこちら</span>
+			</a>
+			<a class="p-front-page__banner">
+				<figure class="c-front-page__banner--image">
+					<img src="/images/no-image.png">
+					<figcaption class="p-front-page__banner--text">
+						<ul class="p-front-page__banner--data">
+							<li class="c-front-page__banner--date">2022/01/13</li>
+							<li class="c-front-page__banner--title">新車入荷しました</li>
+						</ul>
+					</figcaption>
+				</figure>
+				<span class="c-front-page__button">詳細はこちら</span>
+			</a>
+			<a class="p-front-page__banner">
+				<figure class="p-front-page__banner--image">
+					<img src="/images/no-image.png">
+					<figcaption class="p-front-page__banner--text">
+						<ul class="p-front-page__banner--data">
+							<li class="c-front-page__banner--date">2022/01/13</li>
+							<li class="c-front-page__banner--title">新車入荷しました</li>
+						</ul>
+					</figcaption>
+				</figure>
+				<span class="c-front-page__button">詳細はこちら</span>
+			</a>
+			<a class="c-front-page__button"><span class="c-front-page__button--text">ブログ一覧を見る</span></a>
+		</article>
+	</main>
+<?php get_footer(); ?>
