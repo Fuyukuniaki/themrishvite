79a50274c252736b7e9bf3aa998cd1ce9c4513b9
Fuyukuniaki <info@a5-w.com>
2023-04-26 03:32:23 +0900

    html〜header

--- functions.php
+++ functions.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Functions and definitions
--- header.php
+++ header.php
@@ -6,3 +6,7 @@
 <?php wp_head(); ?>
 </head>
 <body>
+	<?php $the_category = get_the_category(); ?>
+	<header id="<?php echo 'o-' . $the_category; ?>">
+		<?php wp_nav_menu(array('menu' => 'mainMenu')); ?>
+	</header>
